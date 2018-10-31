215e8895613e8f5301f0942850a0f540c25fe57d
andreykhorkov <andrey.khorkov@borgward.com>
2018-10-31 14:13:31 -0700

    +

---
+++ Assets/Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+
+    [SerializeField] private Transform plane;
+    [SerializeField] private Transform target;
+    [SerializeField] private float speed = 1;
+    [SerializeField] private float rotationSpeed = 30;
+
+    void Update()
+    {
+        var dir = (target.position - plane.position).normalized;
+        var dotProd = Vector3.Dot(plane.right, dir);
+        plane.Rotate(Vector3.up, dotProd * rotationSpeed * Time.deltaTime);
+        //plane.rotation *= Quaternion.AngleAxis(dotProd * rotationSpeed * Time.deltaTime, Vector3.up);
+
+        plane.position += plane.forward * speed * (1 - dotProd) * Time.deltaTime;
+    }
+}
