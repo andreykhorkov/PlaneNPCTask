154adab4d8e33ab7ec4e3879f2ccb8d9ffffbc3c
andreykhorkov <and.khorkov@gmail.com>
2019-06-26 23:37:20 -0700

    всякая хуета

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class Controller : MonoBehaviour
 {
@@ -13,7 +14,7 @@ public class Controller : MonoBehaviour
         var dir = (target.position - plane.position).normalized;
         var dotProd = Vector3.Dot(plane.right, dir);
         //plane.Rotate(Vector3.up, dotProd * rotationSpeed * Time.deltaTime);
-        plane.rotation *= Quaternion.AngleAxis(dotProd * rotationSpeed * Time.deltaTime, Vector3.up);
+        plane.rotation *= Quaternion.AngleAxis(dotProd * rotationSpeed * Time.deltaTime, plane.up);
 
         plane.position += plane.forward * speed * (1 - dotProd) * Time.deltaTime;
     }
---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+
+    public Transform sphere;
+    public Transform target;
+    public float initialAccel = 20;
+    public float initialSpeed = 200;
+    public float gravity = 9.8f;
+
+    private Vector3 dir;
+    private Vector3 stopDir;
+    private Vector3 velocity;
+
+    // Use this for initialization
+    void Start ()
+    {
+        
+	    dir = (target.position - sphere.position).normalized;
+        stopDir = -Vector3.ProjectOnPlane(dir, Vector3.up);
+        velocity = dir * initialSpeed;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    
+	    velocity += (stopDir * 5 + Vector3.down * gravity) * Time.deltaTime;
+	    sphere.transform.position += velocity * Time.deltaTime;
+	}
+}
